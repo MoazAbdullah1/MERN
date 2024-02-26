7add2677e3c18d4e169f643cabf1ce3ab402792d
MoazAbdullah1 <moazabdullah02@gmail.com>
2024-02-26 20:43:16 +0500

    Add Component And Screen And Navbar Added

---
+++ Food/mern/src/screens/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
+
+export default function () {
+  return (
+    <div>
+      <div>
+        <Navbar />
+      </div>
+      <div>Body</div>
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
+}
